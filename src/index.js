f72f8b0ecbce89f542c36a62ca4ba0f5aba5ad26
Hananehass <hananehajmi@gmail.com>
2023-08-23 22:54:19 +0200

    renommer index.css par index.scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom'; // Utilisez HashRouter au lieu de createHashRouter
-import './index.css';
+import './index.scss';
 import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
 
