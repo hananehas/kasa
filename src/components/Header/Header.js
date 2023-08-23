97beec2471e76cb43f2d2a93296fc2b7091a93d7
Hananehass <hananehajmi@gmail.com>
2023-08-23 16:44:44 +0200

    ajout link
    pour navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,21 @@
 import logo from'../../assets/logo.svg';
 import './Header.scss';
+import {Link} from 'react-router-dom';
+
 
 function Header() {
     return (
         <header className="Header">
             <img src={logo} className="Header-logo" alt="logo" />
             <nav>
+                <ul>   
+                    <li>
+                         <Link to="/">Accueil</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">À propos</Link>
+                    </li>
+                </ul>
             </nav>
         </header>
     );
