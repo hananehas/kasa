9169aa377a16daab7ce29c6eaa489e77d6fa6684
Hananehass <hananehajmi@gmail.com>
2023-08-24 11:34:17 +0200

    coorection font size de navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,14 @@ import './Header.scss';
 import {Link} from 'react-router-dom';
 
 
+
 function Header() {
     return (
         <header className="Header">
             <img src={logo} className="Header-logo" alt="logo" />
             <nav className='Navbar'>
 
-                <Link className={CurrentLocation('/', ' currentLink', 'App-links')} to="/" >Accueil</Link>
+                <Link className='App-link' to="/" >Accueil</Link>
 
                 <Link className="App-link" to="/about">À propos</Link>
 
