57496e61d59b9418f52572a2c9fce02a0d2f4dc6
Hananehass <hananehajmi@gmail.com>
2023-08-30 23:17:50 +0200

    Update Header.jsx
    modification de class une fois le link cliqué

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,24 @@
 import logo from'../../assets/logo.svg';
 import './Header.scss';
 import {Link} from 'react-router-dom';
-
-
+import React, { useState } from 'react';
 
 function Header() {
+    const [isClicked, setIsClicked] = useState(false);
+    const handleClick = () => {
+        setIsClicked(true);
+    };
+   
     return (
         <header className="Header">
             <img src={logo} className="Header-logo" alt="logo" />
             <nav className='Navbar'>
-
-                <Link className='App-link' to="/" >Accueil</Link>
-
-                <Link className="App-link" to="/about">À propos</Link>
-
+                <Link to="/" onClick={handleClick} className={isClicked ? 'clicked-link' : 'App-link' }>
+                    Accueil
+                </Link>
+                <Link to="/about" onClick={handleClick} className={isClicked ? 'clicked-link' : 'App-link'}>
+                    A propos
+                </Link>
             </nav>
         </header>
     );
