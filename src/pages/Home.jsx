aa841e9638daa461060bd39673f516a814e25a91
Hananehass <hananehajmi@gmail.com>
2023-08-30 23:56:36 +0200

    bannière  de la page Home ajoutée

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import Header from '../components/Header/Header'
-import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+import Banner from '../components/Banner/Banner';
+import HomeBanner from '../assets/HomeBanner.png';
 
 
 function Home() {
     return (
-        <div>
+        <div class="container">
             <Header />
+            <Banner img={HomeBanner} text={'Chez vous, partout et ailleurs'}/>
             <div>
                 <Footer />
             </div>
