364402d028717c0de1e37ee4d3ad42c751c3f150
Hananehass <hananehajmi@gmail.com>
2023-08-30 23:14:28 +0200

    Ajouter la bannière

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import Banner from './HomeBanner.png';
+import './Banner';
+
+function Banner(){
+
+    <div>
+        <img src={img} className="Banner" alt="HomeBanner" ></img>
+    </div>
+}
+
+export default Banner
