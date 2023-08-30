91a01d716858fa12eef1fa808af184999bcbe186
Hananehass <hananehajmi@gmail.com>
2023-08-30 17:00:32 +0200

    Update index.js
    utuliser createHashRouter c'est plus simple pour instruction de chaque route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom'; // Utilisez HashRouter au lieu de createHashRouter
-import './index.scss';
-import Home from './pages/Home';
-import reportWebVitals from './reportWebVitals';
-import About from './pages/About';
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <Home />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import ReactDOM from 'react-dom/client';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
 
-reportWebVitals();
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Home />,
+   
+  },
+ 
+  {
+    path: '/about',
+    element: <About />,
+  },
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
