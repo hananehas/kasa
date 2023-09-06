8d3614f267b6d232e6b7c2606b7c5d9a7bee8b2e
Hananehass <hananehajmi@gmail.com>
2023-09-06 15:36:00 +0200

    Update About.jsx
    ajout de banner a la page about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,19 @@
-import Header from '../components/Header/Header'
-
+import Header from '../components/Header/Header';
+import Banner from '../components/Banner/Banner';
+import aboutBanner from '../assets/aboutBanner.png';
+import Footer from '../components/Footer/Footer';
 
 function About() {
 
     return (
-        <Header/>
-
-        
-    )
+        <div>
+            <div className="container">
+                <Header />
+                <Banner img={aboutBanner} />
+            </div>
+            <Footer />
+        </div>
+    );
 }
+
 export default About;
