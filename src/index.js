92a5a4c57bbdf1386761c431bd95b6b1159496a9
Hananehass <hananehajmi@gmail.com>
2023-09-06 15:16:32 +0200

    Update index.js
    ajouter la page error

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
+import Error from './pages/Error.jsx'
 import './index.scss';
 const router = createHashRouter([
   {
     path: '/',
     element: <Home />,
+    errorElement: <Error />,
    
   },
  
