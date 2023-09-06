d129b19b658ec3a079ebc41ef26ac433ece3f907
Hananehass <hananehajmi@gmail.com>
2023-09-06 15:38:37 +0200

    Update About.jsx
    fetch data a partir  du fichier json

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,18 @@
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header/Header';
 import Banner from '../components/Banner/Banner';
 import aboutBanner from '../assets/aboutBanner.png';
 import Footer from '../components/Footer/Footer';
 
 function About() {
+   
+
+    useEffect(() => {
+        fetch('/aboutData.json')
+            .then((response) => response.json())
+            .then((data) => setAboutData(data))
+            .catch((error) => console.error('Erreur  :', error));
+    }, []);
 
     return (
         <div>
@@ -11,6 +20,9 @@ function About() {
                 <Header />
                 <Banner img={aboutBanner} />
             </div>
+            <main>
+               
+            </main>
             <Footer />
         </div>
     );
