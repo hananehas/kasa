905fd912d36f44cce0aab3cbc33904dc85c49d69
Hananehass <hananehajmi@gmail.com>
2023-09-06 15:32:59 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,17 @@
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import Banner from '../components/Banner/Banner';
-import HomeBanner from '../assets/HomeBanner.png';
+import homeBanner from '../assets/homeBanner.png';
 
 
 function Home() {
     return (
-        <div class="container">
-            <Header />
-            <Banner img={HomeBanner} text={'Chez vous, partout et ailleurs'}/>
-            <div>
-                <Footer />
+        <div>
+            <div className="container">
+                <Header />
+                <Banner img={homeBanner} text={'Chez vous, partout et ailleurs'} />
             </div>
-
+            <Footer />
         </div>
 
     )
